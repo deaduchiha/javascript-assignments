a2894fb9ff50d3b2f57eeb5988371fd056bd778f
Alireza Nikzad <alireza.nikzdd@gmail.com>
2023-09-10 10:22:18 +0330

    getting index of text

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // get 4th char
-const text = "Alireza";
-console.log(text[4]); // e
+// const text = "Alireza";
+// console.log(text[4]); // e
+
+// - - - - - - - - - - -
+// index of
+const text = "alireza";
+console.log(text.indexOf("l")); // 1
