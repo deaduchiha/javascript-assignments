a2d6e45bc71e34cd2af2283a8fcc43d62afddc59
Alireza Nikzad <alireza.nikzdd@gmail.com>
2023-09-10 10:23:42 +0330

    charAt

--- index.js
+++ index.js
@@ -3,6 +3,13 @@
 // console.log(text[4]); // e
 
 // - - - - - - - - - - -
+
 // index of
+// const text = "alireza";
+// console.log(text.indexOf("l")); // 1 ** if take -1 its meaning its not exist
+
+// - - - - - - - - - - -
+
+// charAt => find text char with index
 const text = "alireza";
-console.log(text.indexOf("l")); // 1
+console.log(text.charAt(0)); // a
