d4f4259e7485e7ad715750c22ffdeb0eb39fc4bb
Alireza Nikzad <alireza.nikzdd@gmail.com>
2023-09-11 10:39:05 +0330

    update phone

--- assignment 2/phone.js
+++ assignment 2/phone.js
@@ -1,5 +1,6 @@
 const phone = "09182229988";
-const subString = phone.slice(4, 7);
-
-const finalPhone = phone.replace(subString, "***");
+// const subString = phone.slice(4, 7);
+// const sign = "***";
+// const finalPhone = phone.replace(subString, sign);
+const finalPhone = phone.replace(phone.slice(4, 7), "***");
 console.log(finalPhone);
